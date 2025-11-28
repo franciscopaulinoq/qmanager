7036a7f218614c5d171c966ef4e2cd155bb38814
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2025-11-28 19:06:04 -0300

    fix: corrects weight validation

--- src/main/java/io/github/franciscopaulinoq/qmanager/dto/PriorityRequest.java
+++ src/main/java/io/github/franciscopaulinoq/qmanager/dto/PriorityRequest.java
@@ -1,15 +1,17 @@
 package io.github.franciscopaulinoq.qmanager.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Min;
 
 public record PriorityRequest(
         @NotBlank(message = "The prefix is mandatory")
         @Size(min = 1, max = 1, message = "The prefix must be one character")
         String prefix,
 
-        @NotBlank(message = "The weight is mandatory")
-        @Size(min = 1, message = "The weight must be one or more")
+        @NotNull(message = "The weight is mandatory")
+        @Min(value = 1, message = "The weight must be one or more")
         int weight,
 
         @NotBlank(message = "The name is mandatory")
