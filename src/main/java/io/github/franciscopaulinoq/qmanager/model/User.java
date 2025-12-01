794137f1e0203843c77ad4dbbafea732369de44e
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2025-12-01 11:11:53 -0300

    feat: define user model

---
+++ src/main/java/io/github/franciscopaulinoq/qmanager/model/User.java
@@ -0,0 +1,45 @@
+package io.github.franciscopaulinoq.qmanager.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@SuperBuilder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends AbstractBaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private boolean active = true;
+}
