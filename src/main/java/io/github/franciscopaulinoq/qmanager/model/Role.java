c1706b8ec8a8e95dabf86868261fb46b9430e393
Francisco Paulino de Queiroz <franciscopaulino.q@gmail.com>
2026-02-09 14:24:15 -0300

    feat: add permission model

--- src/main/java/io/github/franciscopaulinoq/qmanager/model/Role.java
+++ src/main/java/io/github/franciscopaulinoq/qmanager/model/Role.java
@@ -2,9 +2,13 @@ package io.github.franciscopaulinoq.qmanager.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +17,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -36,4 +42,13 @@ public class Role extends AbstractBaseModel {
     @Builder.Default
     @Column(nullable = false)
     private boolean active = true;
+
+    @Builder.Default
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "roles_permissions",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "permission_id")
+    )
+    private Set<Permission> permissions = new HashSet<>();
 }
--- src/main/java/io/github/franciscopaulinoq/qmanager/repository/UserRepository.java
+++ src/main/java/io/github/franciscopaulinoq/qmanager/repository/UserRepository.java
@@ -2,11 +2,16 @@ package io.github.franciscopaulinoq.qmanager.repository;
 
 import io.github.franciscopaulinoq.qmanager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles r LEFT JOIN FETCH r.permissions WHERE u.email = :email")
+    Optional<User> findByEmailWithRolesAndPermissions(@Param("email") String email);
+
     Optional<User> findByEmail(String email);
     Optional<User> findByIdAndActiveTrue(UUID id);
 }
